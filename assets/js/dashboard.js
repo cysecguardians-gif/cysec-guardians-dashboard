35dc5939a736774ff42168ab4baa860496bcab14
cysecguardians-gif <cysecguardians@gmail.com>
2026-02-14 02:13:22 +0530

    Update dashboard.js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,1 +1,14 @@
+import { apiFetch } from './api.js';
+
+async function loadDashboard() {
+  const data = await apiFetch('/dashboard/summary');
+
+  document.getElementById('total-users').innerText =
+    data.total_users;
+
+  document.getElementById('click-rate').innerText =
+    data.click_rate + '%';
+}
+
+loadDashboard();
 
