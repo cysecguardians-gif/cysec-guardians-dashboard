c965fee89f81cec1224c047b023b4dc9b5f617ce
cysecguardians-gif <cysecguardians@gmail.com>
2026-02-14 02:31:21 +0530

    Create phishing.js

---
+++ assets/js/phishing.js
@@ -0,0 +1,34 @@
+import { apiFetch } from "/assets/js/api.js";
+
+const table = document.getElementById("campaignTable");
+
+function renderCampaigns(data) {
+  table.innerHTML = "";
+
+  if (!data.length) {
+    table.innerHTML =
+      `<tr><td colspan="3" class="text-center">No campaigns found</td></tr>`;
+    return;
+  }
+
+  data.forEach(c => {
+    table.innerHTML += `
+      <tr>
+        <td>${c.name}</td>
+        <td>${c.status}</td>
+        <td>${c.launch_date || ""}</td>
+      </tr>
+    `;
+  });
+}
+
+async function loadCampaigns() {
+  try {
+    const data = await apiFetch("/phishing/campaigns");
+    renderCampaigns(data);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+loadCampaigns();
